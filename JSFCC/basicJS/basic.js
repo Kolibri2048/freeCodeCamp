9a2290f0349052145d76a00aa356bf674d03025e
Kolibri2048 <blak_fog@mail.ru>
2023-04-30 19:56:20 +0200

    feat: solution task

---
+++ JSFCC/basicJS/basic.js
@@ -0,0 +1,34 @@
+//Task 1 Comment your JS code 
+//dsdsd
+/*dsssd*/
+
+// Task 2 Declare JavaScript Variables
+// Use the var keyword to create a variable called myName.
+var myName;
+
+//Task 3    Assign the value 7 to variable a.
+var a; 
+a = 7;
+
+//Task 4 Assign the contents of a to variable b.
+var b;
+b = a;
+
+//Task 5 
+var a = 9;
+
+// Task 6 
+var myFirstName = 'German'
+var myLastName = 'Gres'
+
+// Task 7
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = 'I am a';
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
