9475c3bc93645d3eae3a77fa96b87cf4be226aa2
Kolibri2048 <blak_fog@mail.ru>
2023-05-09 20:12:07 +0200

    feat: solution task TestingObj

--- JSFCC/basicJS/TestingObjectsforProperties.js
+++ JSFCC/basicJS/TestingObjectsforProperties.js
@@ -17,4 +17,12 @@
 // If not, return Not Found.
 
 //Solution
-
+function checkObj(obj, checkProp) {
+    // Only change code below this line
+    if(obj.hasOwnProperty(checkProp)) {
+      return obj[checkProp]
+    } else {
+      return 'Not Found'
+    }
+    // Only change code above this line
+  }
