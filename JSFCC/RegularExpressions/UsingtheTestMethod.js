fc9358a7bcafabd6f3b4f7c5efe9069b2167a522
Kolibri2048 <blak_fog@mail.ru>
2023-05-22 15:46:50 +0200

    feat:solution task

--- JSFCC/RegularExpressions/UsingtheTestMethod.js
+++ JSFCC/RegularExpressions/UsingtheTestMethod.js
@@ -34,3 +34,45 @@
 let myString = "Hello, World!";
 let myRegex = /Hello/;
 let result = myRegex.test(myString); // Change this line
+
+
+// Совпадение с литеральными строками
+// В последнем задании вы искали слово, Helloиспользуя регулярное выражение /Hello/. Это регулярное выражение искало буквальное совпадение строки
+//  Hello. Вот еще один пример поиска буквального совпадения строки Kevin:
+
+// let testStr = "Hello, my name is Kevin.";
+// let testRegex = /Kevin/;
+// testRegex.test(testStr);
+// Этот testвызов вернется true.
+
+// Любые другие формы Kevinне будут соответствовать. Например, регулярное выражение /Kevin/не будет соответствовать kevinили KEVIN.
+
+// let wrongRegex = /kevin/;
+// wrongRegex.test(testStr);
+// Этот testвызов вернется false.
+
+// Будущая задача покажет, как сопоставить и эти другие формы.
+
+
+// Завершите регулярное выражение waldoRegex, чтобы найти "Waldo"в строке waldoIsHidingбуквальное совпадение.
+let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+let waldoRegex = /Waldo/; // Change this line
+let results = waldoRegex.test(waldoIsHiding);
+
+
+//                   Сопоставьте литеральную строку с различными возможностями
+// Используя регулярные выражения, такие как /coding/, вы можете искать шаблон codingв другой строке.
+
+// Это мощный инструмент для поиска отдельных строк, но он ограничен только одним шаблоном. Вы можете искать несколько шаблонов, 
+// используя оператор alternationили OR: |.
+
+// Этот оператор сопоставляет шаблоны либо до, либо после него. Например, если вы хотите сопоставить строки yesили no, вам нужно регулярное
+//  выражение /yes|no/.
+
+// Вы также можете искать более двух шаблонов. Вы можете сделать это, добавив больше шаблонов с большим количеством ORоператоров, разделяющих их,
+//  например /yes|no|maybe/.
+
+// Заполните регулярное выражение petRegex, чтобы оно соответствовало домашним животным dog, cat, birdили fish.
+let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/; // Change this line
+let result = petRegex.test(petString);
